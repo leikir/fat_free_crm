951e80c4c71cfbedb12c431717bd05b2c3f98750
Aureliano Legna <aureliano.legna@leikir-studio.fr>
2015-09-07 10:41:56 +0200

    Intégration categories a field groups

--- db/migrate/20150904131159_add_category_to_account.rb
+++ db/migrate/20150904131159_add_category_to_account.rb
@@ -1,6 +1,5 @@
 class AddCategoryToAccount < ActiveRecord::Migration
   def change
     add_column :field_groups, :category_key, :string
-    add_column :accounts, :category_key, :string
   end
 end
